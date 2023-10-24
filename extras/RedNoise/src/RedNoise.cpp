e7fc5741c932b1685de959d3ebd4666796cb732d
JeeIn-Park <1700pji@naver.com>
2023-10-24 15:05:03 +0100

    outer triangle, need to map texture again

--- extras/RedNoise/src/RedNoise.cpp
+++ extras/RedNoise/src/RedNoise.cpp
@@ -232,34 +232,27 @@ void texturedTriangle(DrawingWindow &window, CanvasTriangle triangle, const std:
     pk.texturePoint = TexturePoint(((p1.texturePoint.y-p0.texturePoint.y)*p2.texturePoint.x + (p2.texturePoint.y-p1.texturePoint.y)*p0.texturePoint.x)/(p2.texturePoint.y-p0.texturePoint.y),p1.texturePoint.y);
 
     flatTriangleTexture(window, p0, pk, p1, texture);
+    Colour white = Colour(255, 255, 255);
+    strokedTriangle(window, triangle, white);
 }
 
 void draw(DrawingWindow &window) {
-//    CanvasPoint v0 = CanvasPoint(160, 10);
-//    CanvasPoint v1 = CanvasPoint(300, 230);
-//    CanvasPoint v2 = CanvasPoint(10, 150);
-//    v0.texturePoint = TexturePoint(195, 5);
-//    v1.texturePoint = TexturePoint(395, 380);
-//    v2.texturePoint = TexturePoint(65, 330);
-//    CanvasTriangle triangle = CanvasTriangle(v0, v1, v2);
-//    texturedTriangle(window, triangle, "/home/jeein/Documents/CG/computer_graphics/extras/RedNoise/src/texture.ppm");
+    CanvasPoint v0 = CanvasPoint(160, 10);
+    CanvasPoint v1 = CanvasPoint(300, 230);
+    CanvasPoint v2 = CanvasPoint(10, 150);
+    v0.texturePoint = TexturePoint(195, 5);
+    v1.texturePoint = TexturePoint(395, 380);
+    v2.texturePoint = TexturePoint(65, 330);
+    CanvasTriangle triangle = CanvasTriangle(v0, v1, v2);
+    texturedTriangle(window, triangle, "/home/jeein/Documents/CG/computer_graphics/extras/RedNoise/src/texture.ppm");
 
 
-//   uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-    uint32_t red = (255 << 24) + (255 << 16) + (0 << 8) + 0;
-    uint32_t green = (255 << 24) + (0 << 16) + (255 << 8) + 0;
-    uint32_t blue = (255 << 24) + (0 << 16) + (0 << 8) + 255;
-    uint32_t white = (255 << 24) + (255 << 16) + (255 << 8) + 255;
+//    uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+//    uint32_t red = (255 << 24) + (255 << 16) + (0 << 8) + 0;
+//    uint32_t green = (255 << 24) + (0 << 16) + (255 << 8) + 0;
+//    uint32_t blue = (255 << 24) + (0 << 16) + (0 << 8) + 255;
+//    uint32_t white = (255 << 24) + (255 << 16) + (255 << 8) + 255;
 
-
-    uint32_t intColour = white;
-
-
-    Colour colour;
-    colour.blue = (intColour >> 0) & 0xFF;       // Extract blue channel (bits 0-7)
-    colour.green = (intColour >> 8) & 0xFF;     // Extract green channel (bits 8-15)
-    colour.red = (intColour >> 16) & 0xFF;     // Extract red channel (bits 16-23)
-    filledTriangle(window, randomTriangle(window), colour);
 }
 
 
